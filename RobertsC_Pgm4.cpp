1dbb00b08daadfb485e12873d4bff2f2250f21de
Connor Roberts <conrobe@bgsu.edu>
2022-10-16 12:28:07 -0400

    Menu and opening files

--- RobertsC_Pgm4.cpp
+++ RobertsC_Pgm4.cpp
@@ -16,13 +16,19 @@
 #include <string>
 #include <fstream>
 #include<stdlib.h>
+#include<windows.h>
+#include<vector>
+
+
 using namespace std;
 
 int main() {
 	ifstream inFS; // Input file steam
 
 	int choice;
+	// Displays menu
 	do {
+	
 		cout << "Select file to process:" << endl;
 		cout << "1. mixed.txt" << endl;
 		cout << "2. valid.txt" << endl;
@@ -31,20 +37,37 @@ int main() {
 		cout << endl;
 		cout << "Choice: ";
 		cin >> choice;
-
-		// If input is 1, read mixed.txt
-		
-		// If input is 2, read valid.txt
-
-		// If input is 3, read three.txt
 		system("cls");
+		
 
-		} while (choice != 4);
+		} while (choice > 4 || choice < 1);
 
-	// 
-	if (choice == 4) {
-		cout << "ERROR: Data file not found";
-	}
+		// Inputs 1-4
+		while (choice < 5 && choice > 0) {
+
+			// If input is 1
+			if (choice == 1) {
+				inFS.open("mixed.txt");
+				break;
+			}
+
+			// If input is 2
+			if (choice == 2) {
+				inFS.open("valid.txt");
+				break;
+			}
+			// If input is 3
+			if (choice == 3) {
+				inFS.open("three.txt");
+				break;
+			}
+
+			// If input is 4
+			if (choice == 4) {
+				cout << "ERROR: Data file not found" << endl;
+				break;
+			}
+		}
 
 
 	return 0;
